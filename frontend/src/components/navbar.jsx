1e4ad4f8d7c197738cbacef0d8ae5a8394e48a49
JohnYncierto <jhyncierto@gmail.com>
2026-04-29 03:12:06 +0800

    downloaded lucide

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -3,8 +3,8 @@ import { Link } from "react-router-dom";
 function Navbar() {
     return (
         <nav className="fixed top-0 left-0 right-0 z-50 flex items-center justify-between px-10 py-5 bg-black/60 backdrop-blur-md border-b border-purple-900/40">
-                <Link to="/" className="text-purple-300 font-bold text-xl tracking-widest uppercase">
-                ✦ Starfall
+                <Link to="/"  className="text-purple-300 font-bold text-xl tracking-widest uppercase hover:text-purple-200 transition-colors duration-300">
+                    Starfall
                 </Link>
                 <div className="flex gap-8">
                     <Link to="/" className="text-purple-200/70 hover:text-purple-300 text-sm tracking-widest uppercase transition-colors duration-300">Home</Link>
