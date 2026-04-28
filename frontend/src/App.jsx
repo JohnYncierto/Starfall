a3f8ddb3d6b7421564a0d24bcf6657b5a546e5e2
JohnYncierto <jhyncierto@gmail.com>
2026-04-29 02:03:31 +0800

    setup App for pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,19 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Rules from './pages/Rules'
+import About from './pages/About'
+import Community from './pages/Community'
+
 function App() {
   return (
-    <div>
-      <h1>Starfall</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/rules" element={<Rules />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/community" element={<Community />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
